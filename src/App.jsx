16a05582a7138c8553954d40d721150b0d8ecc06
DESKTOP-B9UVUDU\Bibi <beniamin.kumala@gmail.com>
2024-04-15 10:01:42 +0200

    Slider mini project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from "./Carousel";
+import SlickCarousel from "./SlickAndCarousel";
+
+const App = () => {
+  return (
+    <main>
+      {/* <Carousel></Carousel> */}
+      <SlickCarousel></SlickCarousel>
+    </main>
+  );
+};
+export default App;
